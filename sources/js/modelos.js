6f31e47848719aafb02180b158dd0033410b0308
Samuel Henares Choaibi <samuelhenares@paucasesnovescifp.cat>
2025-05-03 23:55:30 +0200

    Commit: modelos.js

---
+++ sources/js/modelos.js
@@ -0,0 +1,67 @@
+class ClasificacionGet {
+    constructor(name, puntuacion) {
+        this.data = {
+            name: name,
+            puntuacion: puntuacion
+        };
+    }
+}
+
+class ClasificacionPost {
+    constructor(api_token, name, puntuacion) {
+        this.api_token = api_token;
+        this.name = name;
+        this.puntuacion = puntuacion;
+        this.success = "Classification added successfully";
+    }
+
+    getSuccess() {
+        return this.success;
+    }
+}
+
+class PostsGet {
+    constructor(title, content) {
+        this.data = {
+            title: title,
+            content: content
+        }
+    }
+}
+
+class CommentsGet {
+    constructor(name, content) {
+        this.data = {
+            name: name,
+            content: content
+        }
+    }
+}
+
+class CommentsPost {
+    constructor(api_token, name, content) {
+        this.api_token = api_token;
+        this.name = name;
+        this.content = content;
+        this.success = "Comments added successfully";
+    }
+    
+    getSuccess() {
+        return this.success;
+    }
+}
+
+class ContactPost {
+    constructor(api_token, nombre, email, asunto, mensaje) {
+        this.api_token = api_token;
+        this.nombre = nombre;
+        this.email = email;
+        this.asunto = asunto;
+        this.mensaje = mensaje;
+        this.success = "Message added successfully";
+    }
+
+    getSuccess() {
+        return this.success;
+    }
+}
